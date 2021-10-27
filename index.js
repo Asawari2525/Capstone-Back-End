0fbc00159a2b4bb28f9b24dead971037d5424fba
Asawari2525 <baishflypi255@gmail.com>
2021-10-28 00:12:33 +0530

    Final Checkpoint

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const config = require('config');
+const products = require("./controllers/products");
+const auth = require("./controllers/auth");
+const addresses = require("./controllers/addresses");
+const orders = require("./controllers/orders");
+const users = require("./controllers/users");
 
 mongoose.set('useNewUrlParser',true);
 mongoose.set('useUnifiedToplogy',true);
@@ -13,6 +18,26 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
+if (!config.get("jwtPrivateKey")) {
+    console.error("FATAL ERROR: jwtPrivateKey is not defined");
+    process.exit(1); // 0 means success anything else means failure, so if jwtPrivateKey is not set we get error and process exit...
+  }
+  
+  
+  mongoose
+    .connect("mongodb://localhost/upGrad_Eshop_application")
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((err) => console.error("Couldnot connected to database"));
+  
+  
+  app.use(express.json());
+  app.use("/products", products);
+  app.use("/addresses", addresses);
+  app.use("/orders", orders);
+  app.use("/users", users);
+  app.use("/auth", auth);
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>{
